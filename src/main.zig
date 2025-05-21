632d7048fee8440ea4ccde0117a77f7cc38d1ac5
Nemesty <frederic.duval@minhbo.fr>
2025-05-21 22:58:56 +0200

    Variables et assignement

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,11 @@
 const std = @import("std");
 
 pub fn main() void {
-    std.debug.print("Salut les {s} !", .{"gens"});
+    // ### Variables et assignement ###
+    std.debug.print("⚡ Variable et assignement ⚡\n", .{});
+    const pi = 3.14;
+    var age: u8 = 33;
+    age += 1;
+    std.debug.print("La valeur de PI est de {d}\n", .{pi});
+    std.debug.print("Tu as {d} ans !\n", .{age});
 }
