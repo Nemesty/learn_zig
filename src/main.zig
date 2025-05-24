39a8cb624af64e03d45c2c80a445e6090e4fd00d
Nemesty <frederic.duval@minhbo.fr>
2025-05-24 17:45:41 +0200

    Tableaux

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,15 @@ const std = @import("std");
 
 pub fn main() void {
     // ### Variables et assignement ###
-    std.debug.print("⚡ Variable et assignement ⚡\n", .{});
+    std.debug.print("\n⚡ Variable et assignement ⚡\n", .{});
     const pi = 3.14;
-    var age: u8 = 33;
-    age += 1;
+    var age: u8 = undefined;
+    age = 33 + 1;
     std.debug.print("La valeur de PI est de {d}\n", .{pi});
     std.debug.print("Tu as {d} ans !\n", .{age});
+
+    // ### Tableaux ###
+    std.debug.print("\n⚡ Tableaux ⚡\n", .{});
+    const hey = [3]u8{ "H", "e", "y" };
+    const google = [_]u8{ "G", "o", "o", "g", "l", "e" };
 }
