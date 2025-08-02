50687ae96e80a6e341d9e151c88bf995ef5a16db
Nemesty <frederic.duval@minhbo.fr>
2025-08-02 09:53:26 +0200

    Fonctions et remise du code au propre

--- src/main.zig
+++ src/main.zig
@@ -1,31 +1,42 @@
-// Import
+// --- Import ---
 const std = @import("std");
 
+// --- Fonction sans argument ni retour ---
+fn sayWelcome() void {
+    std.debug.print("\n⚡⚡⚡ Zig, le langage en action ! ⚡⚡⚡\n", .{});
+}
+
+// --- Fonction avec argument sans retour ---
+fn showTitle(title: []const u8) void {
+    std.debug.print("\n\n⚡ {s} ⚡ \n", .{title});
+}
+
 pub fn main() void {
+    sayWelcome();
 
     // --- Variables et constantes ---
     {
-        std.debug.print("\n⚡ Variable et constantes ⚡\n", .{});
+        showTitle("Variable et constantes");
         const pi = 3.14;
         var age: u8 = undefined;
         age = 33;
         age += 1;
         std.debug.print("La valeur de PI est de {d}\n", .{pi});
-        std.debug.print("Tu as {d} ans !\n", .{age});
+        std.debug.print("Tu as {d} ans !", .{age});
     }
 
     // --- Tableaux ---
     {
-        std.debug.print("\n⚡ Tableaux ⚡\n", .{});
+        showTitle("Tableaux");
         const hey = [3]u8{ 'H', 'e', 'y' };
         const google = [_]u8{ 'G', 'o', 'o', 'g', 'l', 'e' };
         std.debug.print("La première lettre du tableau hey est : {c}\n", .{hey[0]});
-        std.debug.print("La longueur du tableau google est : {d}\n", .{google.len});
+        std.debug.print("La longueur du tableau google est : {d}", .{google.len});
     }
 
     // --- Conditions ---
     {
-        std.debug.print("\n⚡ Conditions ⚡\n", .{});
+        showTitle("Conditions");
         var age: i8 = -2;
         age += 1;
         if ((age < 18 and age > 0) or age > 60) {
@@ -38,23 +49,23 @@ pub fn main() void {
         const fullLife = true;
         var lifeBar: u8 = 0;
         lifeBar += if (fullLife) 100 else 50;
-        std.debug.print("Vous avez {d}% de votre santé.\n", .{lifeBar});
+        std.debug.print("Vous avez {d}% de votre santé.", .{lifeBar});
     }
 
     // --- Boucle While ---
     {
-        std.debug.print("\n⚡ Boucle While ⚡\n", .{});
+        showTitle("Boucle While");
         var i: u8 = 0;
         while (i < 12) : (i += 1) {
             if (i % 2 == 0) continue;
             if (i == 11) break;
-            std.debug.print("i = {d}\n", .{i});
+            std.debug.print("i = {d}", .{i});
         }
     }
 
     // --- Boucle For ---
     {
-        std.debug.print("\n⚡ Boucle For ⚡\n", .{});
+        showTitle("Boucle For");
         const pseudo = [_]u8{ 'N', 'e', 'm', 'e', 's', 't', 'y' };
         std.debug.print("Bonjour ", .{});
         for (pseudo, 0..) |character, index| {
@@ -63,6 +74,4 @@ pub fn main() void {
         }
         std.debug.print(" !", .{});
     }
-
-    {}
 }
